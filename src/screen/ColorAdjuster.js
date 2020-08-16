61a1a05771189eb1dceace0934a97c39a35db5be
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-08-16 11:47:46 -0400

    commit

--- src/screen/ColorAdjuster.js
+++ src/screen/ColorAdjuster.js
@@ -13,12 +13,16 @@ export default function ColorAdjuster() {
         //color === red, green, blue
         //change === + 15, - 15
 
-        if(color === "red"){
-            if(red + change > 255 || red + change < 0){
+        switch(color){
+            case "red":
+                red + change > 225 || red + change < 0 ? null : setRed(red + change);
                 return;
-            }else{
-                setRed(red + change)
-            }
+            case "green":
+                green + change > 225 || green + change < 0 ? null : setGreen(geen+ change);
+                return;
+            case "blue":
+                blue + change > 225 || blue + change < 0 ? null : setBlue(blue + change);
+                return;
         }
     }
 
