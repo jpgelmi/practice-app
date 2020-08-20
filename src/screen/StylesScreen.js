84665385601790d48de76143cdcc3c707bc7abdb
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-08-20 17:01:05 -0400

    commit

--- src/screen/StylesScreen.js
+++ src/screen/StylesScreen.js
@@ -3,10 +3,38 @@ import { StyleSheet, Text, View } from 'react-native'
 
 export default function StylesScreen() {
     return (
-        <View>
-            <Text></Text>
+        <View style = {styles.parentStyle}>
+            <View style = {styles.textStyleOne}/>
+            <View style = {styles.textStyleTwo}/>
+            <View style = {styles.textStyleTree}/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    parentStyle:{
+        borderWidth:3,
+        borderColor:"black",
+        height:200,
+        flexDirection:"row",
+        justifyContent:"space-between"
+    },
+    textStyleOne:{
+        backgroundColor:"red",
+        height: 50,
+        width:50
+    },
+    textStyleTwo:{
+        backgroundColor: "green",
+        height: 50,
+        width:50,
+        marginVertical:100
+    },
+    textStyleTree:{
+        backgroundColor:"blue",
+        height: 50,
+        width:50,
+        
+
+    }
+})
